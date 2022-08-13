d516cb01c8d57d7c2fe68cc63914e8ca1bf49232
Chris <chrisiivix@gmail.com>
2022-08-12 23:42:08 -0700

    Enabled hot reload of development server.

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # import requi9red module
 import uvicorn
 
-from app import main
 
 if __name__ == "__main__":
-    uvicorn.run(main.app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
